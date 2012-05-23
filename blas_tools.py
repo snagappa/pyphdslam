8167af811c8858228a3d9131ae5207f67530fd35
snagappa <snagappa@33abe129-c9b7-4dba-9617-cd7109fdd5f0>
2012-05-23 02:09:49 +0000

    updates to the blas wrappers.

--- blas_tools.py
+++ blas_tools.py
@@ -21,6 +21,8 @@ def array(*args, **kw):
     else:
         return np.array(*args, **kw) 
 
+DEBUG = False
+
 
 global_support_code = """
 #include <atlas/cblas.h>
@@ -111,6 +113,30 @@ def whatisit(arr):
         itis = ITIS(True, [], 0, (), type(arr))
         return itis
 
+
+def isvalidvector(x):
+    x_is = whatisit(x)
+    if not (x_is.consistent_type==np.ndarray):
+        return False
+    if not x.flags.c_continuous:
+        return False
+    if not (len(x.shape)==2):
+        return False
+    return True
+    
+def assert_valid_vector(x, varname="unknown"):
+    x_is = whatisit(x)
+    assert (x_is.consistent_type()==np.ndarray), varname + " must be of type ndarray"
+    assert x.flags.c_contiguous, varname + " must be C-order contiguous"
+    assert (len(x.shape)==2), varname + " must be 2 dimensional"
+    
+def assert_valid_matrix(x, varname="unknown"):
+    x_is = whatisit(x)
+    assert (x_is.consistent_type()==np.ndarray), varname + " must be of type ndarray"
+    assert x.flags.c_contiguous, varname + " must be C-order contiguous"
+    assert (len(x.shape)==3), varname + " must be 3 dimensional"
+    
+    
 def isnestedstruct(itis_info):
     if not itis_info.valid:
         return True
@@ -182,7 +208,188 @@ def fast_copy(x):
     return y
     
 
-def dgemv(A, x, alpha=1.0, beta=0.0, y=None, TRANSPOSE_A=False):
+def ddot(x, y):
+    if DEBUG:
+        assert_valid_vector(x, "x")
+        assert_valid_vector(y, "y")
+        assert (x.shape[0] in [1, y.shape[0]]) and (y.shape[0] in [1, x.shape[0]]), "number of elements in x and y must match"
+        assert (x.shape[1] == y.shape[1]), "vectors must have equal lengths"
+    xt_dot_y = np.zeros(max([x.shape[0], y.shape[0]]), dtype=float)
+    weave.inline(blas_tools_c_code.ddot.code,
+                 blas_tools_c_code.ddot.python_vars, 
+                 libraries=blas_tools_c_code.ddot.libraries,
+                 support_code=blas_tools_c_code.ddot.support_code,
+                 extra_compile_args=blas_tools_c_code.EXTRA_COMPILE_ARGS)
+    return xt_dot_y
+    
+    
+def dnrm2(x):
+    if DEBUG:
+        assert_valid_vector(x, "x")
+    
+    nrm2 = np.zeros(x.shape[0], dtype=float)
+    weave.inline(blas_tools_c_code.dnrm2.code,
+                 blas_tools_c_code.dnrm2.python_vars, 
+                 libraries=blas_tools_c_code.dnrm2.libraries,
+                 support_code=blas_tools_c_code.dnrm2.support_code,
+                 extra_compile_args=blas_tools_c_code.EXTRA_COMPILE_ARGS)
+    return nrm2
+
+
+def dasum(x):
+    if DEBUG:
+        assert_valid_vector(x)
+    
+    asum = np.zeros(x.shape[0], dtype=float)
+    weave.inline(blas_tools_c_code.dasum.code,
+                 blas_tools_c_code.dasum.python_vars, 
+                 libraries=blas_tools_c_code.dasum.libraries,
+                 support_code=blas_tools_c_code.dasum.support_code,
+                 extra_compile_args=blas_tools_c_code.EXTRA_COMPILE_ARGS)
+    return asum
+    
+    
+def idamax(x):
+    if DEBUG:
+        assert_valid_vector(x, "x")
+    
+    max_idx = np.empty(x.shape[0], dtype=int)
+    weave.inline(blas_tools_c_code.idamax.code,
+                 blas_tools_c_code.idamax.python_vars, 
+                 libraries=blas_tools_c_code.idamax.libraries,
+                 support_code=blas_tools_c_code.idamax.support_code,
+                 extra_compile_args=blas_tools_c_code.EXTRA_COMPILE_ARGS)
+    return max_idx
+    
+    
+def daxpy(alpha, x, y=None):
+    if DEBUG:
+        assert_valid_vector(x, "x")
+    
+    fn_return_val = None
+    if y == None:
+        y = np.zeros(x.shape)
+        fn_return_val = y
+    if DEBUG:
+        assert_valid_vector(y, "y")
+        assert x.shape == y.shape, "x and y must have the same shape"
+    
+    if np.isscalar(alpha):
+        alpha = np.array([alpha])
+    alpha = alpha.astype(float)
+    
+    if DEBUG:
+        assert len(alpha) in [1, x.shape[0]], "alpha must be a scalar or a numpy array of same length as x"
+    
+    weave.inline(blas_tools_c_code.daxpy.code,
+                 blas_tools_c_code.daxpy.python_vars, 
+                 libraries=blas_tools_c_code.daxpy.libraries,
+                 support_code=blas_tools_c_code.daxpy.support_code,
+                 extra_compile_args=blas_tools_c_code.EXTRA_COMPILE_ARGS)
+    return fn_return_val
+    
+    
+def dscal(alpha, x):
+    if DEBUG:
+        assert_valid_vector(x)
+    
+    if np.isscalar(alpha):
+        alpha = np.array([alpha])
+    alpha = alpha.astype(float)
+    
+    if DEBUG:
+        assert len(alpha) in (1, x.shape[0]), "alpha must be a scalar or a numpy array of same length as x"
+    
+    weave.inline(blas_tools_c_code.dscal.code,
+                 blas_tools_c_code.dscal.python_vars, 
+                 libraries=blas_tools_c_code.dscal.libraries,
+                 support_code=blas_tools_c_code.dscal.support_code,
+                 extra_compile_args=blas_tools_c_code.EXTRA_COMPILE_ARGS)
+    
+
+def dgemv(A, x, alpha=1.0, y=None, beta=1.0, TRANSPOSE_A=False):
+    if DEBUG:
+        assert_valid_matrix(A, "A")
+        assert_valid_vector(x, "x")
+    fn_return_val = None
+    if y==None:
+        y = np.zeros(((max([A.shape[0],x.shape[0]]),) + x.shape[1:]))
+        fn_return_val = y
+    if DEBUG:
+        assert_valid_vector(y, "y")
+    
+    if np.isscalar(alpha):
+        alpha = np.array([alpha], dtype=float)
+    if np.isscalar(beta):
+        beta = np.array([beta], dtype=float)
+    if DEBUG:
+        assert_valid_vector(alpha, "alpha")
+        assert_valid_vector(beta, "beta")
+        assert len(alpha) in [1, x.shape[0]], "alpha must be a scalar or have same length as x"
+        assert (x.shape[0]==A.shape[0]) or (x.shape[0]==1) or (A.shape[0]==1), "incompatible sizes for A and x specified"
+        assert A.shape[2] == x.shape[1], "x must have as many elements as columns in A"
+        assert len(beta) in [1, y.shape[0]], "beta must be a scalar or have same length as y"
+    C_CONTIGUOUS = A.flags.c_contiguous
+    weave.inline(blas_tools_c_code.dgemv.code, 
+                 blas_tools_c_code.dgemv.python_vars, 
+                 libraries=blas_tools_c_code.dgemv.libraries,
+                 support_code=blas_tools_c_code.dgemv.support_code,
+                 extra_compile_args=blas_tools_c_code.EXTRA_COMPILE_ARGS)
+    return fn_return_val
+    
+    
+def dger(x, y, alpha=1.0, A=None):
+    if DEBUG:
+        assert_valid_vector(x, "x")
+        assert_valid_vector(y, "y")
+    
+    fn_return_val = None
+    if A==None:
+        A = np.zeros((max([x.shape[0], y.shape[0]]), x.shape[1], y.shape[1]), dtype=float)
+        fn_return_val = A
+    
+    if DEBUG:
+        assert_valid_matrix(A, "A")
+        assert A.shape == (max([x.shape[0], y.shape[0]]), x.shape[1], y.shape[1]), "x,y are incompatible with A"
+    
+    if np.isscalar(alpha):
+        alpha = np.array([alpha], dtype=float)
+    if DEBUG:
+        assert_valid_vector(alpha, "alpha")
+        assert len(alpha) in [1, x.shape[0], y.shape[0]], "alpha must be a scalar or have same length as x"
+        assert (x.shape[0]==y.shape[0]) or (x.shape[0]==1) or (y.shape[0]==1), "incompatible sizes for x and y specified"
+    weave.inline(blas_tools_c_code.dger.code, 
+                 blas_tools_c_code.dger.python_vars, 
+                 libraries=blas_tools_c_code.dger.libraries,
+                 support_code=blas_tools_c_code.dger.support_code,
+                 extra_compile_args=blas_tools_c_code.EXTRA_COMPILE_ARGS)
+    return fn_return_val
+    
+    
+    
+def dgemm(A, B, alpha=1.0, C=None, beta=1.0, TRANSPOSE_A=False, TRANSPOSE_B=False):
+    assert_valid_matrix(A, "A")
+    assert_valid_matrix(B, "B")
+    
+    fn_return_val = None
+    if C==None:
+        C_rows = A.shape[2] if TRANSPOSE_A else A.shape[1]
+        C_cols = B.shape[1] if TRANSPOSE_B else B.shape[2]
+        C = np.zeros((max([A.shape[0], B.shape[0]]), C_rows, C_cols), dtype=float)
+        fn_return_val = C
+    C_is = whatisit(C)
+    assert C_is.consistent_type()==np.ndarray, "C must by of type ndarray"
+    assert C.flags.c_contiguous, "blas_tools.dgemm may only be used with C-order contiguous data"
+    
+    test_rows = A.shape[2] if TRANSPOSE_A else A.shape[1]
+    test_cols = B.shape[1] if TRANSPOSE_B else B.shape[2]
+    assert C_is.shape == (max([A.shape[0], B.shape[0]]), test_rows, test_cols), "A,B are incompatible with C"
+    
+    return fn_return_val
+
+
+
+def dgemv_old(A, x, alpha=1.0, beta=0.0, y=None, TRANSPOSE_A=False):
     A_is = whatisit(A)
     x_is = whatisit(x)
     A_consistent_type = A_is.consistent_type()
@@ -194,14 +401,28 @@ def dgemv(A, x, alpha=1.0, beta=0.0, y=None, TRANSPOSE_A=False):
         y_consistent_type = y_is.consistent_type
         assert A_consistent_type == y_consistent_type, "A, x and y must have consistent types"
     
-    assert A_consistent_type in [np.ndarray, list, LIST_OF_NDARRAY], "Valid type must be used"
+    assert A_consistent_type in [np.ndarray, list], "Valid type must be used"
     y_is = copy.deepcopy(x_is)
     if not (A_is.shape[0] == x_is.shape[0]):
         y_is.shape = ((max([A_is.shape[0],x_is.shape[0]]),) + x_is.shape[1:])
     
     x_shape = x_is.shape
     A_shape = A_is.shape
+    if np.isscalar(alpha):
+        alpha = np.array([alpha], dtype=float)
+    assert type(alpha) == np.ndarray, "alpha must be of type scalar float or float ndarray"
+    assert len(alpha) in [1, x_shape[0]], "alpha must be a scalar or have same length as x"
     
+    y = zeros(y_is)
+    assert A.flags.c_contiguous or A.flags.f_contiguous, "blas_tools.dgemv may only be used with contiguous data"
+    C_CONTIGUOUS = A.flags.c_contiguous
+    weave.inline(blas_tools_c_code.npdgemv.code, 
+                 blas_tools_c_code.npdgemv.python_vars, 
+                 libraries=blas_tools_c_code.npdgemv.libraries,
+                 support_code=blas_tools_c_code.npdgemv.support_code,
+                 extra_compile_args=blas_tools_c_code.EXTRA_COMPILE_ARGS)
+    return y
+    """
     if A_consistent_type == list:
         y = zeros(y_is)
         weave.inline(blas_tools_c_code.ldgemv.code, 
@@ -220,7 +441,7 @@ def dgemv(A, x, alpha=1.0, beta=0.0, y=None, TRANSPOSE_A=False):
                      support_code=blas_tools_c_code.npdgemv.support_code,
                      extra_compile_args=blas_tools_c_code.EXTRA_COMPILE_ARGS)
                      
-    """
+    
     elif A_consistent_type in [LIST_OF_NDARRAY, LIST_OF_MATRIX]:
         assert A.flags.c_contiguous or A.flags.f_contiguous, "blas_tools.dgemv may only be used with contiguous data"
         
@@ -233,10 +454,75 @@ def dgemv(A, x, alpha=1.0, beta=0.0, y=None, TRANSPOSE_A=False):
                      support_code=blas_tools_c_code.npdgemv.support_code,
                      extra_compile_args=blas_tools_c_code.EXTRA_COMPILE_ARGS)
         y = [y[i] for i in range(len(y))]
-    """
+    
     return y
+    """
     
     
+def test_ddot(num_elements=1000, num_dims=4):
+    print "Computing dot product..."
+    x = np.random.rand(num_elements, num_dims)
+    y = np.random.rand(num_elements, num_dims)
+    # Using numpy:
+    np_xt_dot_y = np.sum(x*y, 1)
+    # Using blas
+    b_xt_dot_y = ddot(x, y)
+    max_err = max(np.abs(np_xt_dot_y-b_xt_dot_y))
+    print "Max error = ", max_err
+    
+def test_dnrm2(num_elements=1000, num_dims=4):
+    print "Computing dot product..."
+    x = np.random.rand(num_elements, num_dims)
+    # Using numpy:
+    np_norm2 = np.array([np.linalg.norm(x[i]) for i in range(num_elements)])
+    # Using blas
+    b_norm2 = dnrm2(x)
+    max_err = max(np.abs(np_norm2-b_norm2))
+    print "Max error = ", max_err
+    
+def test_dasum(num_elements=1000, num_dims=4):
+    print "Computing dot product..."
+    x = np.random.rand(num_elements, num_dims)
+    # Using numpy:
+    np_asum = np.sum(np.abs(x), 1)
+    # Using blas
+    b_asum = dasum(x)
+    max_err = max(abs(np.abs(np_asum-b_asum)))
+    print "Max error = ", max_err
+    
+def test_idamax(num_elements=1000, num_dims=4):
+    print "Computing dot product..."
+    x = np.random.rand(num_elements, num_dims)
+    # Using numpy:
+    np_idx = x.argmax(1)
+    # Using blas
+    b_idx = idamax(x)
+    max_err = max(abs(np.abs(np_idx-b_idx+1)))
+    print "Max error = ", max_err
+    
+def test_daxpy(num_elements=1000, num_dims=4):
+    print "Computing dot product..."
+    a = np.random.rand(num_elements)
+    x = np.random.rand(num_elements, num_dims)
+    y = np.random.rand(num_elements, num_dims)
+    # Using numpy:
+    np_axpy = np.array([a[i]*x[i]+y[i] for i in range(num_elements)]).flatten()
+    # Using blas
+    daxpy(a, x, y)
+    max_err = max(abs(np.abs(np_axpy-y.flatten())))
+    print "Max error = ", max_err
+    
+def test_dscal(num_elements=1000, num_dims=4):
+    print "Computing dot product..."
+    a = np.random.rand(num_elements)
+    x = np.random.rand(num_elements, num_dims)
+    # Using numpy:
+    np_scal = np.array([a[i]*x[i] for i in range(num_elements)]).flatten()
+    # Using blas
+    dscal(a, x)
+    max_err = max(abs(np.abs(np_scal-x.flatten())))
+    print "Max error = ", max_err
+
 def test_dgemv(num_elements=1000, num_dims=4, num_rows=4):
     print "Case 1: num_P == num_x"
     x = np.random.rand(num_elements, num_dims)
--- blas_tools_c_code.py
+++ blas_tools_c_code.py
@@ -5,14 +5,27 @@ Created on Sun May 20 19:05:54 2012
 @author: snagappa
 """
 
-EXTRA_COMPILE_ARGS = ["-O2"]
+EXTRA_COMPILE_ARGS = ["-O3"]
 
-global_support_code = """
+gsl_blas_headers = """
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+
+"""
+f77_blas_headers = """
 #include <atlas/cblas.h>
 #include <atlas/clapack.h>
 
 extern "C" {
+    double ddot_(int *N, double *X, int *INCX, double *Y, int *INCY);
+    double dnrm2_(int *N, double *X, int *INCX);
+    double dasum_(int *N, double *X, int *INCX);
+    int idamax_(int *N, double *X, int *INCX);
+    int daxpy_(int *N, double *ALPHA, double *X, int *INCX, double *Y, int *INCY);
+    int dscal_(int *N, double *ALPHA, double *X, int *INCX);
     int dgemv_(char *TRANS, int *M, int *N, double *ALPHA, double *A, int *LDA, double *X, int *INCX, double *BETA, double *Y, int *INCY);
+    //int dger_(int *M, )
 }
 
 inline void copylist1d(py::indexed_ref x, int ncols, double *x_copy) {
@@ -71,35 +84,117 @@ class lcopy:
     """
     
 
-# dsdot -- inner product of two vectors
-
-
+###############################################################################
 # ddot -- dot product of two vectors
+class ddot:
+    def __call__(self):
+        return python_vars, code, support_code, libs
+    support_code = f77_blas_headers
+    libraries = ["blas", "lapack", "gsl"]
+    python_vars = ["x", "y","xt_dot_y"]
+    code = """
+    int i, num_x, num_y, max_num;
+    int vec_len, x_offset, y_offset, inc;
+    //gsl_vector_view gsl_x, gsl_y;
+    inc = 1;
+    num_x = Nx[0];
+    vec_len = Nx[1];
+    num_y = Ny[0];
+    max_num = num_x>num_y?num_x:num_y;
+    
+    x_offset = num_x==1?0:vec_len;
+    y_offset = num_y==1?0:vec_len;
+    
+    for (i=0; i<max_num; i++) {
+        //gsl_x = gsl_vector_view_array(x+(i*x_offset), vec_len);
+        //gsl_y = gsl_vector_view_array(y+(i*y_offset), vec_len);
+        //gsl_blas_ddot (&gsl_x.vector, &gsl_y.vector, xt_dot_y+i);
+        xt_dot_y[i] = ddot_(&vec_len, x+(i*x_offset), &inc, y+(i*y_offset), &inc);
+    }
+    """
 
 
+###############################################################################
 # dnrm2 -- Euclidean norm of a vector
+class dnrm2:
+    def __call__(self):
+        return python_vars, code, support_code, libs
+    support_code = global_support_code
+    libraries = ["blas", "lapack"]
+    python_vars = ["x", "nrm2"]
+    code = """
+    int i, num_x;
+    int vec_len, inc;
+    
+    inc = 1;
+    num_x = Nx[0];
+    vec_len = Nx[1];
+    
+    for (i=0; i<num_x;i++)
+        nrm2[i] = dnrm2_(&vec_len, x+(i*vec_len), &inc);
+    """
 
-
+    
+###############################################################################
 # dasum -- sum of absolute values
+class dasum:
+    def __call__(self):
+        return python_vars, code, support_code, libs
+    support_code = f77_blas_headers
+    libraries = ["blas", "lapack"]
+    python_vars = ["x", "asum"]
+    code = """
+    int i, num_x;
+    int vec_len, inc;
+    
+    inc = 1;
+    num_x = Nx[0];
+    vec_len = Nx[1];
+    
+    for (i=0; i<num_x;i++)
+        asum[i] = dasum_(&vec_len, x+(i*vec_len), &inc);
+    """
 
 
 # idamax -- find index of element having the max absolute value
+class idamax:
+    def __call__(self):
+        return python_vars, code, support_code, libs
+    support_code = f77_blas_headers
+    libraries = ["blas", "lapack"]
+    python_vars = ["x", "max_idx"]
+    code = """
+    int i, num_x;
+    int vec_len, inc;
+    
+    inc = 1;
+    num_x = Nx[0];
+    vec_len = Nx[1];
+    
+    for (i=0; i<num_x;i++)
+        max_idx[i] = idamax_(&vec_len, x+(i*vec_len), &inc);
+    """
 
 
 # daxpy -- y = ax+y
 class daxpy:
     def __call__(self):
         return python_vars, code, support_code, libs
-    support_code = global_support_code
+    support_code = f77_blas_headers
     libraries = ["blas", "lapack"]
     python_vars = ["alpha", "x", "y"]
     code = """
-    int inc;
-    int num_x;
+    int i, num_x, num_alpha;
+    int vec_len, x_offset, alpha_offset, inc;
     
     inc = 1;
     num_x = Nx[0];
-    daxpy_(&num_x, &alpha, x, &inc, y, &inc);
+    vec_len = Nx[1];
+    num_alpha = Nalpha[0];
+    alpha_offset = !(num_alpha==1);
+        
+    for (i=0; i<num_x;i++)
+        daxpy_(&vec_len, alpha+(i*alpha_offset), x+(i*vec_len), &inc, y+(i*vec_len), &inc);
     """
     
 
@@ -107,21 +202,187 @@ class daxpy:
 class dscal:
     def __call__(self):
         return python_vars, code, support_code, libs
-    support_code = global_support_code
+    support_code = f77_blas_headers
     libraries = ["blas", "lapack"]
     python_vars = ["alpha", "x"]
     code = """
-    int inc;
-    int num_x;
+    int i, num_x, num_alpha;
+    int vec_len, x_offset, alpha_offset, inc;
     
     inc = 1;
     num_x = Nx[0];
-    dscal_(&num_x, &alpha, x, &inc);
+    vec_len = Nx[1];
+    num_alpha = Nalpha[0];
+    
+    alpha_offset = !(num_alpha==1);
+    for (i=0; i<num_x;i++)
+        dscal_(&vec_len, alpha+(i*alpha_offset), x+(i*vec_len), &inc);
     """
 
 
 ###############################################################################
+##
+## LEVEL 2 BLAS Functions
+##
+###############################################################################
+
+###############################################################################
 # dgemv -- y = alpha*A*x + beta*y OR y = alpha*A**T*x + beta*y
+class dgemv:
+    def __call__(self):
+        return python_vars, code, support_code, libs
+    support_code = f77_blas_headers
+    libraries = ["blas", "lapack"]
+    python_vars = ["A", "x", "y", "alpha", "beta", "TRANSPOSE_A", "C_CONTIGUOUS"]
+    code = """
+    int i, num_A, num_x, num_alpha, num_beta, max_num;
+    int A_offset, x_offset, alpha_offset, beta_offset, inc;
+    int nrows, ncols;
+    char f_transA;
+    
+    // Row major
+    if ((int)C_CONTIGUOUS)
+        f_transA = (int)TRANSPOSE_A>0? 'n' : 't';
+    // Column major (Fortran)
+    else
+        f_transA = (int)TRANSPOSE_A>0? 't' : 'n';
+    
+    inc = 1;
+    num_A = NA[0];
+    num_x = Nx[0];
+    num_alpha = Nalpha[0];
+    num_beta = Nbeta[0];
+    
+    nrows = NA[1];
+    ncols = NA[2];
+    
+    A_offset = num_A==1?0:nrows*ncols;
+    x_offset = num_x==1?0:ncols;
+    alpha_offset = !(num_alpha==1);
+    beta_offset = !(num_beta==1);
+    
+    max_num = num_A>num_x?num_A:num_x;
+    for (i=0; i<max_num; i++)
+        dgemv_(&f_transA, &nrows, &ncols, alpha+(i*alpha_offset), 
+               A+(i*A_offset), &nrows, x+(i*x_offset), &inc, beta+(i*beta_offset), 
+               y+(i*ncols), &inc);
+    """
+
+
+###############################################################################
+# dsymv -- y = alpha*A*x + beta*y, A is symmetric
+
+
+###############################################################################
+# dger -- rank 1 operation, A = alpha*x*y**T + A
+class dger:
+    def __call__(self):
+        return python_vars, code, support_code, libs
+    support_code = gsl_blas_headers
+    libraries = ["blas", "lapack", "gsl"]
+    python_vars = ["A", "x", "y", "alpha"]
+    code = """
+    // A is stored in row major order and there is no option to transpose so we 
+    // use gsl_blas to perform this
+    
+    int i, num_x, num_y, num_alpha, num_A;
+    int nrows, ncols, x_vec_len, x_offset, y_vec_len, y_offset, alpha_offset, A_offset, inc;
+    gsl_matrix_view gsl_A;
+    gsl_vector_view gsl_x;
+    gsl_vector_view gsl_y;
+    
+    inc = 1;
+    num_x = Nx[0];
+    num_y = Ny[0];
+    num_alpha = Nalpha[0];
+    
+    x_vec_len = Nx[1];
+    y_vec_len = Ny[1];
+    
+    nrows = NA[1];
+    ncols = NA[2];
+    
+    x_offset = num_x==1?0:x_vec_len;
+    y_offset = num_y==1?0:y_vec_len;
+    alpha_offset = !(num_alpha==1);
+    A_offset = nrows*ncols;
+    
+    
+    for (i=1; i<num_A; i++) {
+        gsl_A = gsl_matrix_view_array(A+(i*A_offset), nrows, ncols);
+        gsl_x = gsl_vector_view_array(x+(i*x_offset), x_vec_len);
+        gsl_y = gsl_vector_view_array(y+(i*y_offset), y_vec_len);
+        gsl_blas_dger (alpha[i*alpha_offset], x, y, A.matrix);
+    }
+        //dger(&nrows, &ncols, alpha+(i*alpha_offset), x+(i*x_offset), &inc, 
+        //     y+(i*y_offset), &inc, A+(i*A_offset), &nrows);
+    """
+
+###############################################################################
+# dsyr -- symmetric rank 1 operation, A = alpha*x*x**T + A
+
+
+###############################################################################
+# dsyr2 -- symmetric rank 2 operation, A = alpha*x*y**T + alpha*y*x**T + A
+
+
+###############################################################################
+##
+## Level 3 BLAS
+##
+###############################################################################
+
+###############################################################################
+# dgemm -- matrix-matrix operation, C := alpha*op( A )*op( B ) + beta*C,
+#           where  op( X ) is one of
+#           op( X ) = X   or   op( X ) = X**T
+class dgemm:
+    def __call__(self):
+        return python_vars, code, support_code, libs
+    support_code = global_support_code
+    libraries = ["blas", "lapack", "gsl"]
+    python_vars = ["A", "B", "C", "alpha", "beta"]
+    code = """
+    int i, num_A, num_B, num_C, num_alpha, num_beta;
+    int A_rows, A_cols, B_rows, B_cols, C_rows, C_cols;
+    int A_offset, B_offset, C_offset, alpha_offset, beta_offset;
+    
+    num_A = NA[0]; A_rows = NA[1]; A_cols = NA[2];
+    num_B = NB[0]; B_rows = NB[1]; B_cols = NB[2];
+    num_C = NC[0]; C_rows = NC[1]; C_cols = NC[2];
+    num_alpha = Nalpha[0];
+    num_beta = Nbeta[0];
+    
+    A_offset = num_A==1?0:A_rows*A_cols;
+    B_offset = num_B==1?0:B_rows*B_cols;
+    C_offset = num_C==1?0:C_rows*C_cols;
+    alpha_offset = !(num_alpha==1);
+    beta_offset = !(num_beta==1);
+    
+    for (i=0; i<num_C; i++) {
+        gsl_A = gsl_matrix_view_array(A+(i*A_offset), A_rows, A_cols);
+        gsl_B = gsl_matrix_view_array(B+(i*B_offset), B_rows, B_cols);
+        gsl_C = gsl_matrix_view_array(C+(i*C_offset), C_rows, C_cols);
+        gsl_blas_dgemm (TransA, TransB, alpha[i*alpha_offset], &A.matrix, 
+                        &B.matrix, beta[i*beta_offset], &C.matrix);
+    }
+    """
+
+###############################################################################
+# dsymm -- matrix-matrix operation, A is symmetric
+#           C := alpha*A*B + beta*C, or
+#           C := alpha*B*A + beta*C
+
+
+###############################################################################
+# dsyrk -- symmetric rank k operation, C is symmetric
+#           C := alpha*A*A**T + beta*C, or
+#           C := alpha*A**T*A + beta*C
+
+
+
+###############################################################################
+###############################################################################
 class ldgemv:
     def __call__(self):
         return python_vars, code, support_code, libs
@@ -189,14 +450,14 @@ class ldgemv:
         }
     }
     else {
-        exception_occured = 1;
+        exception_occurred = 1;
     }
     free(A_copy);
     free(x_copy);
     free(y_copy);
     """
     
-class npdgemv:
+class npdgemv_old_plus_broken_code_now:
     def __call__(self):
         return python_vars, code, support_code, libs
     support_code = global_support_code
@@ -204,7 +465,7 @@ class npdgemv:
     python_vars = ["A", "x", "y", "alpha", "beta", "TRANSPOSE_A", "C_CONTIGUOUS"]
     code = """
     int i, j, k, base_offset, inc;
-    int num_A, num_x;
+    int num_A, num_x, num_alpha, A_offset, x_offset, alpha_offset, max_num;
     int nrows, ncols;
     CBLAS_ORDER order;
     CBLAS_TRANSPOSE transA;
@@ -223,15 +484,23 @@ class npdgemv:
     inc = 1;
     num_A = NA[0];
     num_x = Nx[0];
+    num_alpha = Nalpha[0];
     
     nrows = NA[1];
     ncols = NA[2];
     
-    base_offset = nrows*ncols;
+    A_offset = num_A==1?0:nrows*ncols;
+    x_offset = num_x==1?0:ncols;
+    alpha_offset = !(num_alpha==1);
+    
+    max_num = num_A>num_x:num_A:num_x;
+    for (i=0; i<max_num; i++)
+        //cblas_dgemv(order, transA, nrows, ncols, alpha, A+(i*A_offset), nrows, x+(i*ncols), 1, beta, y+(i*ncols), 1);
+        dgemv_(&f_transA, &nrows, &ncols, &alpha, A+(i*A_offset), &nrows, x+(i*ncols), &inc, &beta, y+(i*ncols), &inc);
     if (num_A == num_x) {
         for (i=0; i<num_A; i++)
-            //cblas_dgemv(order, transA, nrows, ncols, alpha, A+(i*base_offset), nrows, x+(i*ncols), 1, beta, y+(i*ncols), 1);
-            dgemv_(&f_transA, &nrows, &ncols, &alpha, A+(i*base_offset), &nrows, x+(i*ncols), &inc, &beta, y+(i*ncols), &inc);
+            //cblas_dgemv(order, transA, nrows, ncols, alpha, A+(i*A_offset), nrows, x+(i*ncols), 1, beta, y+(i*ncols), 1);
+            dgemv_(&f_transA, &nrows, &ncols, &alpha, A+(i*A_offset), &nrows, x+(i*ncols), &inc, &beta, y+(i*ncols), &inc);
     }
     else if (num_A == 1) {
         for (i=0; i<num_x; i++)
@@ -240,39 +509,10 @@ class npdgemv:
     }
     else if (num_x == 1) {
         for (i=0; i<num_A; i++)
-            //cblas_dgemv(order, transA, nrows, ncols, alpha, A+(i*base_offset), nrows, x, 1, beta, y+(i*ncols), 1);
-            dgemv_(&f_transA, &nrows, &ncols, &alpha, A+(i*base_offset), &nrows, x, &inc, &beta, y+(i*ncols), &inc);
+            //cblas_dgemv(order, transA, nrows, ncols, alpha, A+(i*A_offset), nrows, x, 1, beta, y+(i*ncols), 1);
+            dgemv_(&f_transA, &nrows, &ncols, &alpha, A+(i*A_offset), &nrows, x, &inc, &beta, y+(i*ncols), &inc);
     }
     else {
-        exception_occured = 1;
+        exception_occurred = 1;
     }
     """
-
-# dsymv -- y = alpha*A*x + beta*y, A is symmetric
-
-
-# dger -- rank 1 operation, A = alpha*x*y**T + A
-
-
-# dsyr -- symmetric rank 1 operation, A = alpha*x*x**T + A
-
-
-# dsyr2 -- symmetric rank 2 operation, A = alpha*x*y**T + alpha*y*x**T + A
-
-
-
-## Level 3 BLAS
-
-# dgemm -- matrix-matrix operation, C := alpha*op( A )*op( B ) + beta*C,
-#           where  op( X ) is one of
-#           op( X ) = X   or   op( X ) = X**T
-
-
-# dsymm -- matrix-matrix operation, A is symmetric
-#           C := alpha*A*B + beta*C, or
-#           C := alpha*B*A + beta*C
-
-
-# dsyrk -- symmetric rank k operation, C is symmetric
-#           C := alpha*A*A**T + beta*C, or
-#           C := alpha*A**T*A + beta*C
